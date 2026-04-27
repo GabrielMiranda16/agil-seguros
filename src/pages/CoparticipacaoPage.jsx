faae70a8db730289772ed78295c7b0cf4cd129b2
GabrielMiranda16 <gabriel.smiranda161193@gmail.com>
2026-04-27 11:40:48 -0300

    fix: cor e texto do botão Registrar na coparticipação

--- src/pages/CoparticipacaoPage.jsx
+++ src/pages/CoparticipacaoPage.jsx
@@ -695,8 +695,8 @@ const CoparticipacaoPage = () => {
               <Button variant="outline" size="sm" onClick={() => { setImportTipo(tipo); setImportStep('upload'); setImportedRows([]); setIsImportModalOpen(true); }} disabled={!selectedCompanyId}>
                 <Upload className="mr-2 h-4 w-4" /> Importar
               </Button>
-              <Button onClick={() => handleAddClick(tipo)} className="bg-blue-600 hover:bg-blue-700 text-white" disabled={!selectedCompanyId}>
-                <Plus className="mr-2 h-4 w-4" /> Registrar
+              <Button onClick={() => handleAddClick(tipo)} className="bg-[#003580] hover:bg-[#002060] text-white" disabled={!selectedCompanyId}>
+                Registrar
               </Button>
             </div>
           </div>
